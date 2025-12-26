cacb942ca8e410726f9be235201f3d0bba843b65
Will Nourse <wrn13@case.edu>
2025-12-26 12:09:38 -0500

    Saved

--- 0_HelloWorld.py
+++ 0_HelloWorld.py
@@ -1,4 +1,8 @@
 import gymnasium as gym
+from stable_baselines3 import PPO
+from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3.common.monitor import Monitor
 
 env = gym.make("LunarLander-v3")    # create the environment
 observation, info = env.reset()    # reset the environment and get the initial observation
@@ -24,3 +28,24 @@ print("Sample observation", env.observation_space.sample()) # Get a random obser
 print("\n _____ACTION SPACE_____ \n")
 print("Action Space Shape", env.action_space.n)
 print("Action Space Sample", env.action_space.sample()) # Take a random action
+
+env.close()
+
+env = gym.make("LunarLander-v3", n_envs=16)
+env.reset()
+model = PPO(
+    policy = 'MlpPolicy',
+    env = env,
+    n_steps = 1024,
+    batch_size = 64,
+    n_epochs = 4,
+    gamma = 0.999,
+    gae_lambda = 0.98,
+    ent_coef = 0.01,
+    verbose=1)
+
+# Train it for 1,000,000 timesteps
+model.learn(total_timesteps=1000000)
+# Save the model
+model_name = "ppo-LunarLander-v3"
+model.save(model_name)
